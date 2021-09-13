f0b9c63abf78088d9eb6e548fe8ed97a968d9be6
Adrian Bowman <adrian.bowman@glasgow.ac.uk>
2021-09-13 18:54:21 +0100

    Update of version number for development version of the package.

--- sm/NEWS.md
+++ sm/NEWS.md
@@ -1,3 +1,7 @@
+# sm 2.2-6.0
+
+* Details
+
 # sm 2.2-5.7
 
 * The package can now be installed on systems which do not have the tcltk package available.  Some minor big fixes are also included.
--- sm/R/zzz.R
+++ sm/R/zzz.R
@@ -24,7 +24,7 @@
 {
     ## we can't do this in .onLoad
     unlockBinding(".sm.Options", asNamespace("sm"))
-    packageStartupMessage("Package 'sm', version 2.2-5.7: type help(sm) for summary information")
+    packageStartupMessage("Package 'sm', version 2.2-6.0: type help(sm) for summary information")
     invisible()
 }
 
